40834234653e4cdd10f9c11faa6bd81af8d42db8
Kovalenko Pavel <kovalenkopasha1997@gmail.com>
2020-05-01 01:28:13 +0300

    Feature: add feedback block, preloader and create functionality for form

--- src/components/Contact-form/Contact-form.js
+++ src/components/Contact-form/Contact-form.js
@@ -1,1 +1,42 @@
-export const FormValidate = () => {};
+export const FormValidate = () => {
+  const form = document.querySelector(".contact-form");
+  let formContactClassList = [
+    "contact-form--inactive",
+    "contact-form--shown",
+    "contact-form--hide",
+  ];
+  const feedbackBlock = document.querySelector(".feedback");
+  let feedbackBlockClassList = ["feedback--hide", "feedback--shown"];
+  const feedbackButton = document.querySelector(".feedback__button");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    form.classList.add("contact-form--inactive");
+
+    function showFeedback() {
+      formContactClassList.map((item, index) =>
+        index === 2 ? form.classList.add(item) : form.classList.remove(item)
+      );
+      form.reset();
+      feedbackBlockClassList.map((item, index) =>
+        index === 1
+          ? feedbackBlock.classList.add(item)
+          : feedbackBlock.classList.remove(item)
+      );
+    }
+
+    setTimeout(showFeedback, 2000);
+  });
+
+  feedbackButton.addEventListener("click", () => {
+    feedbackBlockClassList.map((item, index) =>
+      index === 0
+        ? feedbackBlock.classList.add(item)
+        : feedbackBlock.classList.remove(item)
+    );
+    formContactClassList.map((item, index) =>
+      index === 1 ? form.classList.add(item) : form.classList.remove(item)
+    );
+  });
+};
