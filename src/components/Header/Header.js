709a94dfab781a36df6f18ce176ca53f52433495
Kovalenko Pavel <kovalenkopasha1997@gmail.com>
2020-05-01 04:34:52 +0300

    Feature: add mobile version header menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,2 +1,7 @@
+import $ from "jquery";
 export const Header = () => {
+  $(".burger").click(function () {
+    $(".header__nav").slideToggle();
+    $(this).toggleClass("burger--active");
+  });
 };
