5bcbaea38a678a382dc4edd00fd58d722fbefdd4
Kovalenko Pavel <kovalenkopasha1997@gmail.com>
2020-05-01 12:36:58 +0300

    fix/burger logiс

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,13 @@
-import $ from "jquery";
 export const Header = () => {
-  $(".burger").click(function () {
-    $(".header__nav").slideToggle();
-    $(this).toggleClass("burger--active");
+  const Burger = document.querySelector(".burger");
+  const HeaderMenu = document.querySelector(".header__nav");
+  Burger.addEventListener("click", () => {
+    if (!HeaderMenu.classList.contains("header__nav--open")) {
+      HeaderMenu.classList.add("header__nav--open");
+      Burger.classList.add("burger--active");
+    } else {
+      HeaderMenu.classList.remove("header__nav--open");
+      Burger.classList.remove("burger--active");
+    }
   });
 };
